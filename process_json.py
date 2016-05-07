e206c443f96d719da31c0ab79d942e1d2dd1c6f0
José María Mateos <chema@rinzewind.org>
2016-05-07 10:25:27 -0400

    Do not create tag pairs

--- process_json.py
+++ process_json.py
@@ -4,6 +4,7 @@ import json
 import pandas
 import gzip
 from itertools import combinations
+from collections import defaultdict
 import pandas as pd
 
 def parse_date(date):
@@ -29,6 +30,18 @@ def get_tag_pairs(tags):
     """Returns all the possible 2-tag combination"""
     tags = sorted([x.lower() for x in tags])
     return([x for x in combinations(tags, 2)])
+    
+def get_relations(opeds, tag):
+    """
+    Returns a count of the relations used with a particular tag.
+    """
+    count = defaultdict(int)
+    tags = [x["tags"] for x in opeds if tag in x["tags"]]
+    for group in tags:
+        for t in group:
+            count[t] += 1
+            
+    return count
 
 if __name__ == "__main__":
     with gzip.open("/home/chema/Dropbox/data/cubazuela/elpais_opeds_full.json.gz", "r") as f:
@@ -38,13 +51,13 @@ if __name__ == "__main__":
     processed_opeds = []
     filter_tags = (u"opinión", )
     for oped in opeds:
-        date = parse_date(oped["date"])
-        tags = [x.lower() for x in oped["tags"] if x.lower() not in filter_tags]
-        tag_pairs = get_tag_pairs(tags)
-        for tag_pair in tag_pairs:
-            processed_opeds.append({"date": date, "title": oped["title"], "url": oped["url"],
-                                    "tag_pair_1": tag_pair[0], "tag_pair_2": tag_pair[1]})
+        oped["date"] = parse_date(oped["date"])
+        oped["tags"] = [x.lower() for x in oped["tags"] if x.lower() not in filter_tags]
+        #tag_pairs = get_tag_pairs(tags)
+        #for tag_pair in tag_pairs:
+        #    processed_opeds.append({"date": date, "title": oped["title"], "url": oped["url"],
+        #                            "tag_pair_1": tag_pair[0], "tag_pair_2": tag_pair[1]})
 
     # Convert to Pandas object and dump to CSV file
-    processed_opeds = pd.DataFrame(processed_opeds)
-    processed_opeds.to_csv("/tmp/elpais_opeds.csv", index = False, encoding = "utf8")
+    #processed_opeds = pd.DataFrame(processed_opeds)
+    #processed_opeds.to_csv("/tmp/elpais_opeds.csv", index = False, encoding = "utf8")
