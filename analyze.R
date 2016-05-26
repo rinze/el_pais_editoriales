1a4039d4d48ae144c3ed548ea2fad932a16e3636
José María Mateos <chema@rinzewind.org>
2016-05-26 19:58:57 -0400

    First truly working version

--- analyze.R
+++ analyze.R
@@ -2,38 +2,54 @@ library(rjson)
 library(dplyr)
 library(ggplot2)
 
-opeds <- fromJSON(file = "elpais_opeds_full.json.gz")
+if(!exists("tags")) {
+    opeds <- fromJSON(file = "elpais_opeds_full.json.gz")
 
-# Create a data.frame with the relevant information
-Sys.setlocale("LC_TIME", "es_ES.UTF-8")
-tags <- lapply(opeds, function(x) {
-            date <- x$date
-            date <- strsplit(date, "-")
-            date <- trimws(date[[1]][1])
-            date <- as.Date(date, "%d %b %Y")
-            dd <- data.frame(date = as.character(date),
-                             tags = as.character(x$tags))
-            return(dd)
-})
+    # Create a data.frame with the relevant information
+    Sys.setlocale("LC_TIME", "es_ES.UTF-8")
+    tags <- lapply(opeds, function(x) {
+                date <- x$date
+                date <- strsplit(date, "-")
+                date <- trimws(date[[1]][1])
+                date <- as.Date(date, "%d %b %Y")
+                dd <- data.frame(date = as.character(date),
+                                 tags = x$tags,
+                                 url = x$url)
+                return(dd)
+    })
 
-tags <- do.call(rbind, tags)
+    tags <- do.call(rbind, tags)
 
-# Year column
-tags$year <- strftime(tags$date, "%Y")
+    # Year column
+    tags$year <- strftime(tags$date, "%Y")
+}
+
+tags$tags <- as.character(tags$tags)
+tags$url <- as.character(tags$url)
 
 # Get number of editorials published per year
-#n_year <- tags %>% group_by(year) %>% summarise(n_year = n())
-#tags <- left_join(tags, n_year)
+n_year <- tags %>% group_by(year) %>% summarise(n_year = length(unique(url)))
+tags <- left_join(tags, n_year)
+
+def <- expand.grid(tags = unique(tags$tags), year = unique(tags$year), 
+                   stringsAsFactors = FALSE)
+def$def <- 0
 
 # Count
 counts <- tags %>% 
     group_by(year, tags) %>% 
-    # Get the absolute count and a normalized count so we can include 2016
-    summarise(count = n())#,
-    #          wcount = n() / n_year) #%>%
-    #arrange(desc(count)) #%>%
-    #top_n(10)
+    summarise(count = n())
 
-plt1 <- ggplot(filter(counts, tags %in% c("Economía", "Venezuela", "Podemos"))) + 
-        geom_point(aes(x = year, y = count, color = tags), stat = "identity")
+counts <- left_join(counts, n_year)
+counts$count_n <- counts$count / counts$n_year * 100
+
+# Add default 0
+counts <- left_join(def, counts)
+counts[is.na(counts)] <- 0
+
+plt1 <- ggplot(filter(counts, tags %in% c("Corrupción", "Chavismo", "Venezuela", "Podemos", "ETA"))) + 
+        geom_line(aes(x = year, y = count_n, color = tags, group = tags)) +
+        theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+        ggtitle("Porcentaje de editoriales dedicados por El País,\n por año, a...") +
+        xlab("Año") + ylab("Porcentaje")
 print(plt1)
